3c203e15c25f4111ceec49431bccc8667182aca9
Noransaber <noransaber685@gmail.com>
2024-01-11 14:07:34 +0200

    Fixing the style at file 2-floot

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -9,6 +9,6 @@ def floor(a: float) -> float:
     """
     floor annotanted function: takes one argument
     a: float
-    return: the floor of the float 
+    return: the floor of the float
     """
     return math.floor(a)
