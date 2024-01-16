c00c01a2ea47de2d79241c5f6acf18343f8ffa59
Noransaber <noransaber685@gmail.com>
2024-01-17 00:22:02 +0300

    Fixing task 0

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -10,4 +10,4 @@ async def async_generator() -> Generator[float, None, None]:
     then yield a random number between 0 and 10"""
     for _ in range(0, 10):
         await asyncio.sleep(1)
-        then yield random.uniform(0, 10)
+        yield random.uniform(0, 10)
